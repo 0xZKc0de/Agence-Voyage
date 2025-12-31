b4da55edd57769b9528aed803c3ffe707383c738
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-31 22:03:11 +0100

    Start working in entities

--- back-end/src/main/java/com/demo/backend/Entity/Circuit.java
+++ back-end/src/main/java/com/demo/backend/Entity/Circuit.java
@@ -1,4 +1,32 @@
 package com.demo.backend.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "circuit")
 public class Circuit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String distination;
+
+    @Column(nullable = false)
+    private Date dateDepart;
+
+    @Column(nullable = false)
+    private Date dateArrive;
+
+    @Column(nullable = false)
+    private double prix;
+
 }
