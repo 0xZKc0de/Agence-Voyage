eb32a287be25da99d974298eb88d195df34cd83a
chifae jbari <chifaejbari2002@gmail.com>
2026-01-03 01:47:17 +0100

    Layout + icons

--- front-end/src/app/admin/admin-layout/admin-layout.component.ts
+++ front-end/src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  standalone: true, // <-- mark as standalone
+  imports: [RouterOutlet, RouterLink, RouterLinkActive], // <-- import router directives
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css']
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent { }
